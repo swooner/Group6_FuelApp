aa94393caeacef2ef990f3fb0cdf26e5104c6bd1
Noble Egbulefu <nobleegbulefu@gmail.com>
2021-06-24 00:13:33 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'bundle.client.js',
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map', 
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: { compact: false }
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'styleTag',
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: true,
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]',
+                            },
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        port: 3000,
+        hot: true,
+        inline: true,
+    },
+};
